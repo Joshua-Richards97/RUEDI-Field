f9e20b3973f24677ecf9058f659526f5c2f48fac
Joshua-Richards97 <joshuaparisrichards@gmail.com>
2026-03-23 12:36:36 +0100

    Add files via upload

---
+++ reudi-service-worker.js
@@ -0,0 +1,33 @@
+const CACHE_VERSION = 'reudi-field-v1';
+const ASSETS = [
+  './',
+  './reudi-field.html',
+  './reudi-manifest.json',
+  './reudi-icon-192.png',
+  './reudi-icon-512.png'
+];
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE_VERSION).then(cache => cache.addAll(ASSETS)));
+  self.skipWaiting();
+});
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE_VERSION).map(k => caches.delete(k)))
+  ));
+  self.clients.claim();
+});
+self.addEventListener('fetch', e => {
+  if (e.request.method !== 'GET') return;
+  e.respondWith(
+    caches.match(e.request).then(cached => {
+      if (cached) return cached;
+      return fetch(e.request).then(response => {
+        if (response && response.status === 200) {
+          const clone = response.clone();
+          caches.open(CACHE_VERSION).then(cache => cache.put(e.request, clone));
+        }
+        return response;
+      }).catch(() => cached);
+    })
+  );
+});
